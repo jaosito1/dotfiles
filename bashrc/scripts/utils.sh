22c9776f5625e1a48c0dc26d06c0ea8cceca95a7
jaosito1 <josuealdana1@gmail.com>
2025-06-29 19:58:42 -0600

    Edited sound script to utilize fallback

--- bashrc/scripts/utils.sh
+++ bashrc/scripts/utils.sh
@@ -6,27 +6,31 @@ vol ()
        return 1
     else 
         local VOLUME=$1
-        local SINK=$(pactl list short sinks | awk '/RUNNING/{print $1;exit} /SUSPENDED/{print $1;exit} /IDLE/{print $1;exit}')
-        if [[ -z "$SINK" ]];
+        local SINK=$(pactl list short sinks | awk '/RUNNING/{print $1;exit} ')
+        local FALLBACK=$( pactl list short sinks | awk '/SUSPENDED/{print $1;exit} /IDLE/{print $1;exit}' )
+
+        if [[ -z "$SINK" && -n "$FALLBACK" ]]; 
+        then
+            SINK="$FALLBACK"
+        elif [[ -z "$SINK" && -z "$FALLBACK" ]];
         then
             echo "no audio running or suspended :("
             return 1
+        fi
+
+        if [[ "$VOLUME" = "m" ]]; 
+        then 
+            pactl set-sink-mute $SINK toggle 
+            echo "(Un)Muted sink $SINK."
         else
-            if [[ "$VOLUME" = "m" ]]; 
-            then 
-                pactl set-sink-mute $SINK toggle 
-                echo "(Un)Muted sink $SINK."
-            else
-                pactl set-sink-volume $SINK $VOLUME%
-                echo "Changed volume to $VOLUME% for sink $SINK."
-            fi
+            pactl set-sink-volume $SINK $VOLUME%
+            echo "Changed volume to $VOLUME% for sink $SINK."
         fi
     fi
 }
 
 br () 
-{
-    if [[ -z $1 ]];
+{ if [[ -z $1 ]];
     then 
         echo "Please select an option (set or get)."
         return 1 
