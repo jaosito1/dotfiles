ea861c59213e3e664acadf765658db291a8e8e85
jaosito1 <josuealdana1@gmail.com>
2025-07-25 14:46:58 -0600

    feat: install script now stows config files

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
-export XDG_CONFIG_HOME=$HOME/.config/
+#!/usr/bin/env bash
 
-stow config/ -t $XDG_CONFIG_HOME
+export XDG_CONFIG_HOME=$HOME/.config
+
+CONFIGDIR="$(pwd)/config"
+pushd "$CONFIGDIR" > /dev/null || exit 1 
+
+for d in *; do 
+    if [ -d "$d"  ]; then
+        stow "$d" -t "$XDG_CONFIG_HOME" || echo "Failed to stow $d"
+    fi
+done
+
+popd > /dev/null
+
+# verify that omp file is created
+
+OMPDIR="$HOME/.local/share/omp"
+
+mkdir -p "$OMPDIR"
+rm -f "$OMPDIR"/*.omp.json
+
+ln -s "$(pwd)/assets/omp-themes/main.omp.json" "$OMPDIR/main.omp.json" 
