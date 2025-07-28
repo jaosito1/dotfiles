9956e23c991f8fa4b880f61655f08855a80bbc71
jaosito1 <josuealdana1@gmail.com>
2025-07-27 22:52:14 -0600

    fix: fixed install script not stowing config files correctly

--- install.sh
+++ install.sh
@@ -3,42 +3,47 @@
 # -- Setup paths
 export XDG_CONFIG_HOME=$HOME/.config
 
-ROOTDIR="$(pwd)"
-CONFIGDIR="$ROOTDIR/config"
-SCRIPTSDIR="$ROOTDIR/scripts"
+root_dir="$(pwd)"
+scripts_dir="$root_dir/scripts"
+
+is_root=$(basename "$root_dir")
+if [[ "$is_root" != "dotfiles" ]]; then
+    echo "Please run the install script form the wsl directory directly ^^"
+    exit 0
+fi
 
 # -- Config files
-pushd "$CONFIGDIR" > /dev/null || exit 1 
-
-for d in *; do 
-    if [ -d "$d"  ]; then
-        stow "$d" -t "$XDG_CONFIG_HOME" || echo "Failed to stow $d"
-    fi
-done
-
-popd > /dev/null
+stow config/ --target="$XDG_CONFIG_HOME" || exit 1
+echo "Symlinked all config files to $XDG_CONFIG_HOME"
 
 # -- Create OMP directory and import layout
+omp_dir="$HOME/.local/share/omp"
+mkdir -p "$omp_dir"
+rm -f "$omp_dir"/*.omp.json
 
-OMPDIR="$HOME/.local/share/omp"
-
-mkdir -p "$OMPDIR"
-rm -f "$OMPDIR"/*.omp.json
-
-ln -sf "$ROOTDIR/assets/omp-themes/main.omp.json" "$OMPDIR/main.omp.json" 
+ln -sf "$root_dir/assets/omp-themes/main.omp.json" "$omp_dir/main.omp.json" 
+echo "Created OMP directory at $omp_dir"
 
 # -- Import scripts
-
-pushd "$SCRIPTSDIR" > /dev/null || exit 1 
+pushd "$scripts_dir" > /dev/null || exit 1 
 
 shopt -s nullglob
-for s in *.sh; do 
+for s in *; do 
+    if [[ "$s" == *.sh ]]; then
+        echo "$s was not imported, please verify your scripts."
+        exit 0
+    fi
     chmod +x "$s"
 done
 shopt -u nullglob
 
 popd > /dev/null
 
-if [[ "$ROOTDIR" == "$HOME/dotfiles" ]]; then
+
+curr_dir=$(basename $(pwd))
+if [[ "$(basename $(pwd))" == "dotfiles" ]]; then
     stow scripts -t "$HOME/.local/bin" || echo "Failed to stow scripts"
+    echo "All scripts were stored succesfully."
+    exit 0
 fi
+
