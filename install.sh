aa0d8b9818c5786e1a3a3fde2a95159533a7154f
jaosito1 <josuealdana1@gmail.com>
2025-07-28 19:34:44 -0600

    feat(initial-zsh): create config files for zsh

--- install.sh
+++ install.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
 
-# -- Setup paths
-home_config=$HOME/.config
+# -- check if script is in dotfiles dir
 root_dir="$(pwd)"
-scripts_dir="$root_dir/scripts"
 
 is_root=$(basename "$root_dir")
 if [[ "$is_root" != "dotfiles" ]]; then
-    echo "Please run the install script form the wsl directory directly ^^"
+    echo "Please run the install script form the dotfiles directory directly ^^"
     exit 0
 fi
 
-# -- Config files
-stow config/ --target="$home_config" || exit 1
-echo "Symlinked all config files to $home_config"
+# -- create dirs
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
 
-# -- Import scripts
+touch $XDG_CACHE_HOME/wget-hsts
+mkdir -p $XDG_CONFIG_HOME/zsh
+mkdir -p $XDG_CONFIG_HOME/shell
+mkdir -p $XDG_DATA_HOME/gnupg
+
+ln -sf $root_dir/zsh/zprofile $HOME/.zprofile
+ln -sf $root_dir/zsh/zshrc $XDG_CONFIG_HOME/zsh/.zshrc
+ln -sf $root_dir/zsh/aliasrc $XDG_CONFIG_HOME/shell/aliasrc
+echo "Symlinked all zsh files"
+
+# -- import scripts
+scripts_dir="$root_dir/scripts"
 pushd "$scripts_dir" > /dev/null || exit 1 
 
 shopt -s nullglob
@@ -35,6 +45,9 @@ curr_dir=$(basename $(pwd))
 if [[ "$(basename $(pwd))" == "dotfiles" ]]; then
     stow scripts -t "$HOME/.local/bin" || echo "Failed to stow scripts"
     echo "All scripts were stored succesfully."
-    exit 0
 fi
 
+# -- stow config files
+stow config/ --target="$XDG_CONFIG_HOME" || exit 1
+echo "Symlinked all config files to $XDG_CONFIG_HOME"
+exit 0
